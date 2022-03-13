ac38ca3cb8915c8ca27d5cd6f3b022c0afc832ef
woohyeonwoo <>
2022-03-13 21:00:17 +0900

    server setting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-const { App } = require('@slack/bolt');
+import { App, ExpressReceiver } from '@slack/bolt'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PROT = 3000
+
 const app = new App({
-  token: ``,
-  signingSecret: ``
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 })
+
+app.command(`/attendance_add`, async ({ say, ack, body, context, command }) => {
+  console.log(body)
+});
 
 (async () => {
   // アプリを起動します
-  await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!');
-})()
+  await app.start(process.env.PORT || PROT)
+  console.log(`⚡️ Bolt app is running! ${PROT}`)
+})();
